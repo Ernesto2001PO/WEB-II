4a0c77b3a3913a54a4c177e9d5ab0641ba089cce
ErnestoNur <111402458+Ernesto2001PO@users.noreply.github.com>
2025-03-27 01:12:50 -0400

    Subiendo cambios del formulario

---
+++ practicaweb/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+const db = require("./models/");
+
+app.set("view engine", "ejs");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+
+
+require("./controllers")(app, db);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
